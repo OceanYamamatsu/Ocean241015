ce5668dd3886b3a4ff373b56f24233169ea0961f
OceanYamamatsu <22211379oy@tama.ac.jp>
2024-12-11 22:32:17 +0900

    a

---
+++ 24後期/Failed to start Docker Application Conta.sql
@@ -0,0 +1,26 @@
+Failed to start Docker Application Container Engine.
+Error: failed to start daemon: Error initializing network controller: could not delete the default bridge network: network bridge has active endpoints
+
+
+$hashed_pwd = sha1($input_pwd);
+$sql = "UPDATE credential SET
+nickname='$input_nickname',
+email='$input_email',
+address='$input_address',
+Password='$hashed_pwd',
+PhoneNumber='$input_phonenumber'
+WHERE ID=$id;";
+$conn->query($sql);
+
+'
+' , salary = 10 WHERE ID = 5; -- 
+
+'
+
+UPDATE credential SET
+nickname='' , salary = 10 WHERE ID = 5; -- 
+email='...',
+address='...',
+Password='...',
+PhoneNumber='...'
+WHERE ID=$id;
