b2c46dfe83ca4e7ba877e70c6373915901ff9551
OceanYamamatsu <22211379oy@tama.ac.jp>
2025-04-28 11:15:52 +0900

    q

--- GoogleDiffusion/AI00/ATagMaker.py
+++ GoogleDiffusion/AI00/ATagMaker.py
@@ -2,11 +2,9 @@
 def extract_tags(input_file, output_file):
     with open(input_file, 'r', encoding='utf-8') as f:
         lines = f.readlines()
-
     tags = []
     skip_words = {"Characters", "General", "?", "artist name",
-                   "twitter username", "copyright notice", "watermark", }
-
+"twitter username", "copyright notice", "watermark", }
     for line in lines:
         line = line.strip()
         if not line or line in skip_words:
@@ -16,7 +14,6 @@ def extract_tags(input_file, output_file):
             parts = line.rsplit(' ', 1)
             tag = parts[0]
             tags.append(tag)
-
     # 結果をoutput.txtに書き込み
     with open(output_file, 'w', encoding='utf-8') as f:
         f.write(", " + ", ".join(tags) + ",")
