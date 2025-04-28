737a1e751e0fc07acbef7e864ff2cb5e7696bfc1
OceanYamamatsu <22211379oy@tama.ac.jp>
2025-04-28 11:14:31 +0900

    s

--- GoogleDiffusion/AI00/ATagMaker.py
+++ GoogleDiffusion/AI00/ATagMaker.py
@@ -11,16 +11,15 @@ def extract_tags(input_file, output_file):
         line = line.strip()
         if not line or line in skip_words:
             continue
-        # 数値が最後についてるので、末尾の数値部分を削除してタグ名だけにする
+        # 数値が最後についてるので、末尾の数値部分を削除してタグ名のみに
         if ' ' in line:
             parts = line.rsplit(' ', 1)
             tag = parts[0]
             tags.append(tag)
 
-    # 結果をoutput.txtに書き込む
+    # 結果をoutput.txtに書き込み
     with open(output_file, 'w', encoding='utf-8') as f:
         f.write(", " + ", ".join(tags) + ",")
 
-# 使用例
 extract_tags('C:/Users/hekat/py/Ocean241015/GoogleDiffusion/AI00/input.txt', 
              'C:/Users/hekat/py/Ocean241015/GoogleDiffusion/AI00/output.txt')
