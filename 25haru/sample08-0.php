8204b4d32f7cb5024d6648aa4e38a7c5c13f9d3a
OceanYamamatsu <22211379oy@tama.ac.jp>
2025-06-25 00:09:41 +0900

    h

---
+++ 25haru/sample08-0.php
@@ -0,0 +1,19 @@
+<!DOCTYPE 
+<html lage="ja" dir="ltr">
+
+
+$myid = 'u22211379';
+$mypass = 'webpg2024';
+$mydb = 'webpg_u22211379';
+
+$db = new mysqli('iis.edu.tama.ac.jp', $myid, $mypass, $mydb);
+
+<p>
+// write your code here
+print( $db->host_info );
+print(1+1)
+//
+</p>
+
+$db->close();
+
