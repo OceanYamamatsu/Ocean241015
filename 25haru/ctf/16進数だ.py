952172bd269bb96606d420b30b40eb216b60b280
OceanYamamatsu <22211379oy@tama.ac.jp>
2025-06-25 22:40:16 +0900

    ｋｎ

---
+++ 25haru/ctf/16進数だ.py
@@ -0,0 +1,10 @@
+# 例：133d - 12a7 のように16進数で計算したい場合に使用しよう
+
+# hex1 = "0x12a7"
+# hex2 = "0x133d"
+# print("A", hex(int(hex1, 16) - int(hex2, 16)))
+
+# 0x5a87be53033d
+hex1 = "0x5a87be53033d"
+hex2 = "0x96"
+print("A", hex(int(hex1, 16) - int(hex2, 16)))
