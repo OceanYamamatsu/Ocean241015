57b356144ac2abf35d33bf02f8ba8fd8d49f83fd
OceanYamamatsu <22211379oy@tama.ac.jp>
2025-07-01 11:05:54 +0900

    a

--- 25haru/webpro/k003a.php
+++ 25haru/webpro/k003a.php
@@ -9,14 +9,17 @@
 ini_set('display_errors',"on");//error表示（本番では表示すべきでない）
 require_once('myps.php');//取り出し
 $db = new mysqli('iis.edu.tama.ac.jp', $myid, $mypass, $mydb);//開き
-// write my code here↓
+// write my code here↓--------------------------------------------------------------
+
+$Puid = '22211379';//ホルダ１
+$Pbody = '何とかホルダー完全に理解した２';//ホルダ２
+$sql = ("insert into messages (uid,body) values (?,?)");//sql草案？
+$stmt = $db->prepare($sql);//sql準備
+$stmt->bind_param("ss", $Puid, $Pbody);//準備したsqlに値を挿入（合成）
+$stmt->execute();//sql実行
+
 //----------------------------------------------------------------------------------
-
-
-$db->query("insert into messages (uid,body) values (22211379,'TEST')");
-
-echo 'Ok';
-//----------------------------------------------------------------------------------
+echo 'Ok';//止まらなかったら出る
 $db->close();//締め
 ?>
     </body>
--- 25haru/webpro/myps.php
+++ 25haru/webpro/myps.php
@@ -1,5 +1,6 @@
 <?php
 $myid = 'u22211379';
 $mypass = 'webpg2024';
-$mydb = 'webpg_u22211379';
+//$mydb = 'webpg_u22211379';//専用
+$mydb = 'webpg_shared';//教養
 ?>
