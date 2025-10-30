b768710e44a98faec0ff68e27165d1872432da39
OceanYamamatsu <22211379oy@tama.ac.jp>
2025-10-30 13:45:42 +0900

    a

---
+++ GoogleDiffusion/kab.md
@@ -0,0 +1,2 @@
+https://minkara.carview.co.jp/userid/681135/car/2518289/5086568/note.aspx
+
--- tmgit/nul01.py
+++ tmgit/nul01.py
@@ -3,3 +3,8 @@ print(int(3.44*2))
 print(1.2*3)
 
 print('q',int(100/3.44))
+i=1
+p=1
+while i <= 10**100:
+    i+=p
+    print(p)
