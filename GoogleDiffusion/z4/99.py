b49c93986f46712c4cc18bceaac72c7c115ebaf7
OceanYamamatsu <22211379oy@tama.ac.jp>
2025-11-26 23:07:22 +0900

    、

--- GoogleDiffusion/z4/99.py
+++ GoogleDiffusion/z4/99.py
@@ -1,23 +1,26 @@
-print(len("VllRAlMDBAEAAlJbVQAABwULAABTX1IAVFldWQsBVg1TBAUA"))
-import base64
-print(base64.b64decode(b'VllRAlMDBAEAAlJbVQAABwULAABTX1IAVFldWQsBVg1TBAUA').decode())
-
-
-
-# print(len("e852b4f2-37c0-4403-8df1-faea8805d41b"))
+# print(len("VllRAlMDBAEAAlJbVQAABwULAABTX1IAVFldWQsBVg1TBAUA"))
 # import base64
-# print(base64.b64decode(b'e852b4f2-37c0-4403-8df1-faea8805d41b').decode())
+# print(base64.b64decode(b'VllRAlMDBAEAAlJbVQAABwULAABTX1IAVFldWQsBVg1TBAUA').decode())
 
-# https://beginner.celtf.com/stage/linux/filesystem/3
-# 病院暗号化偽造
-# MICE
-# money ideology coercion ego
-a = (52//2)**2*3.14*52
-print(a)
 
-a=52
-b=52
-x=3.14/4 * a**2 * b /1000
-print(x)
-print(int(3.14 /4 *a**2 *b /1000))
-print(int(1000*88*4/(3.14*52**2)))
+
+# # print(len("e852b4f2-37c0-4403-8df1-faea8805d41b"))
+# # import base64
+# # print(base64.b64decode(b'e852b4f2-37c0-4403-8df1-faea8805d41b').decode())
+
+# # https://beginner.celtf.com/stage/linux/filesystem/3
+# # 病院暗号化偽造
+# # MICE
+# # money ideology coercion ego
+# a = (52//2)**2*3.14*52
+# print(a)
+
+# a=52
+# b=52
+# x=3.14/4 * a**2 * b /1000
+# print(x)
+# print(int(3.14 /4 *a**2 *b /1000))
+# print(int(1000*88*4/(3.14*52**2)))
+print(5*6)
+
+
