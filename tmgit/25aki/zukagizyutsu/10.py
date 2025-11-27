f4ea57cdd70f3fede774b057c51aff01104c9109
OceanYamamatsu <22211379oy@tama.ac.jp>
2025-11-27 16:01:41 +0900

    j

---
+++ tmgit/25aki/zukagizyutsu/10.py
@@ -0,0 +1,22 @@
+# print(36/15)
+# print(100/2.4)
+# print(100/1.19)
+# print(100/0.98)
+
+# 又は
+# print("a =", [i*i for i in range(8)])
+# 又は
+ 
+x = 1
+i = 1
+a = [0] * 8
+try:
+    for _ in range(8):
+        a[i] = i * i
+        i = i+1
+except IndexError as e:
+    print("a =", a)
+    print("i =", i)
+    print(""" "error" """,e)
+
+
